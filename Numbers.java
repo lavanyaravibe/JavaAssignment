790de933314467caf78bcb40dcac546c58f2d0bb
lavanyaravibe <53295569+lavanyaravibe@users.noreply.github.com>
2019-07-25 14:29:43 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,29 @@
+package firstproject;
+
+import java.util.Scanner;
+public class Numbers {
+	
+public static Scanner scan= new Scanner(System.in);
+public static void main(String[] args)
+{
+	
+	int value=0;
+	int index=1;
+	int linenumber=0;
+	System.out.println("Enter any number from 1 to 100");
+	value=scan.nextInt();
+	System.out.println();
+	
+	do {
+		linenumber++;
+		for(int i=1;i<=linenumber;i++)
+		{
+			System.out.print(index+ " ");
+			index++;
+		}
+		System.out.println();
+	
+	}while((linenumber+index)<=value);
+
+}
+}
